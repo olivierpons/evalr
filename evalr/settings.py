7c76474ad41769804965a11550501321d7b1889b
Olivier Pons <olivier.pons@gmail.com>
2020-04-13 20:01:29 +0200

    settings.py update.

--- evalr/settings.py
+++ evalr/settings.py
@@ -1,28 +1,94 @@
 import os
 import re
 import sys
+from pathlib import Path
 
 from django.utils.translation import gettext_lazy as _
+from collections.abc import Mapping
 
-environment_variables = {
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# region - custom settings (to put in environment settings) -
+class LazyDict(Mapping):
+    def __init__(self, *args, **kw):
+        self._raw_dict = dict(*args, **kw)
+
+    def __getitem__(self, key):
+        if key.startswith('#'):
+            func, arg = self._raw_dict.__getitem__(key)
+            return func(arg)
+        return self._raw_dict.__getitem__(key)
+
+    def __iter__(self):
+        return iter(self._raw_dict)
+
+    def __len__(self):
+        return len(self._raw_dict)
+
+
+settings = {}
+errors = []
+
+
+def str_parser(error_message_if_doesnt_exist):
+    global settings
+    return [str, lambda v: (settings['DEBUG'] is True) or Path(v).is_dir(),
+            error_message_if_doesnt_exist]
+
+
+def conf_ignore_if_sqlite():
+    return {
+        'default': 'None',
+        'parser': [eval,
+                   lambda v:
+                   (isinstance(v, str) and v != '') or
+                   (v is None and 'sqlite' in settings['DATABASE_ENGINE']),
+                   "Your database isn't sqlite, this var must be configured"]}
+
+
+environment_variables = LazyDict({
     # SECURITY WARNING: don't run with debug turned on in production!
-    'DEBUG': {'required': True, 'parser': bool},
+    'DEBUG': {'required': True,
+              'parsers': [eval, lambda v: isinstance(v, bool)]},
     'SECRET_KEY': {'required': True, },
     'MEDIA_ROOT': {'default': 'uploads'},
-    'COMPRESS_ROOT': {'default': 'static/production'},
-    # set it to None = no limit to be able to send huge amount of data:
-    'DATA_UPLOAD_MAX_NUMBER_FIELDS': {'required': True, 'parser': eval},
+    'STATIC_ROOT': {
+        'default': '../production_static_files',
+        'parser': str_parser("Production folder doesn't exist.")},
+    'COMPRESS_ROOT': {
+        'default': '../production_static_files/compress',
+        'parser': str_parser("Compress production folder doesn't exist.")},
+    # when set to None = no limit to be able to send huge amount of data:
+    'DATA_UPLOAD_MAX_NUMBER_FIELDS': {
+        'required': True,
+        'parser': [eval, lambda v: v is None or isinstance(v, int)]},
     # for uploads
     'UPLOAD_FOLDER_DOCUMENTS': {'default': 'documents'},
+    'UPLOAD_FOLDER_CHATS_DOCUMENT': {'default': 'chats/documents'},
+    'UPLOAD_FOLDER_IMAGES': {'default': 'images'},
+    'THUMBNAIL_SUBDIRECTORY': {'default': 'th'},
+    'THUMBNAIL_DIMENSIONS': {
+        'default': '(1125, 2436)',  # iPhone X resolution
+        'parser': [eval, lambda v: (type(v) is tuple) and len(v) == 2]},
+    'ALLOWED_HOSTS': {
+        'default': '[]', 'required': True,  # default = no hosts
+        'parser': [eval,
+                   lambda tab: isinstance(tab, list) and all([
+                       isinstance(x, str) for x in tab])]},  # ! array of str
 
     # DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2' :
-    'DATABASE_ENGINE': {'required': True, },
-    'DATABASE_NAME': {'required': True, },
-    'DATABASE_HOST': {'required': True, },
-    'DATABASE_CLIENT_ENCODING': {'required': True, },
-    'DATABASE_DATABASE': {'required': True, },
-    'DATABASE_USER': {'required': True, },
-    'DATABASE_PASSWORD': {'required': True, },
+    # DATABASE_ENGINE = 'django.db.backends.sqlite3',
+    'DATABASE_ENGINE': {'default': 'django.db.backends.sqlite3', },
+    'DATABASE_NAME': {'default': os.path.join(BASE_DIR, 'db.sqlite3'), },
+
+    'DATABASE_HOST': conf_ignore_if_sqlite(),
+    'DATABASE_CLIENT_ENCODING': conf_ignore_if_sqlite(),
+    'DATABASE_DATABASE': conf_ignore_if_sqlite(),
+    'DATABASE_USER': conf_ignore_if_sqlite(),
+    'DATABASE_PASSWORD': conf_ignore_if_sqlite(),
+
     'SOCIAL_AUTH_GITHUB_KEY': {'required': True, },
     'SOCIAL_AUTH_GITHUB_SECRET': {'required': True, },
     'SOCIAL_AUTH_TWITTER_KEY': {'required': True, },
@@ -34,9 +100,8 @@ environment_variables = {
                                                   'parser': eval},
     'SOCIAL_AUTH_GOOGLE_OAUTH2_KEY': {'required': True, },
     'SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET': {'required': True, },
-}
-settings = {}
-errors = []
+})
+
 for var, infos in environment_variables.items():
     if var in os.environ:
         settings[var] = os.environ[var]
@@ -46,13 +111,38 @@ for var, infos in environment_variables.items():
         errors.append(var)
         continue
     if 'parser' in infos:
-        settings[var] = infos['parser'](settings[var])
+        parser = infos['parser']
+        if var not in settings:
+            raise Exception(f"{var}: variable not set in environment, and "
+                            "has not 'default' or 'required' value")
+        # ! parser functions: 0 = convert, 1 = validate conversion, 2 = error:
+        try:
+            settings[var] = parser[0](settings[var])
+        except TypeError:
+            raise Exception(f"{var}: conversion error using {parser[0]}")
+        if not parser[1](settings[var]):  # should not continue -> exception:
+            if len(parser) > 2:
+                raise Exception(f'{var=} : {parser[2]}')
+            raise Exception(f'Unexpected conversion for variable {var}')
 
 if len(errors):
     raise Exception("Please set the environment variables: "
                     "{}.".format(', '.join(errors)))
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = settings['MEDIA_ROOT']
+COMPRESS_ROOT = settings['COMPRESS_ROOT']
+SECRET_KEY = settings['SECRET_KEY']
+DEBUG = settings['DEBUG']
+DATA_UPLOAD_MAX_NUMBER_FIELDS = settings['DATA_UPLOAD_MAX_NUMBER_FIELDS']
+UPLOAD_FOLDER_DOCUMENTS = settings['UPLOAD_FOLDER_DOCUMENTS']
+UPLOAD_FOLDER_CHATS_DOCUMENT = settings['UPLOAD_FOLDER_CHATS_DOCUMENT']
+UPLOAD_FOLDER_IMAGES = settings['UPLOAD_FOLDER_IMAGES']
+THUMBNAIL_SUBDIRECTORY = settings['THUMBNAIL_SUBDIRECTORY']
+THUMBNAIL_DIMENSIONS = settings['THUMBNAIL_DIMENSIONS']
+ALLOWED_HOSTS = settings['ALLOWED_HOSTS']
+STATIC_ROOT = settings['STATIC_ROOT']
+# endregion - custom settings (to put in environment settings) -
+
 # make Windows-compatible
 if sys.platform.lower().startswith('win'):
     os.environ["PATH"] = os.environ["PATH"] + os.pathsep + os.sep.join([
@@ -62,7 +152,7 @@ if sys.platform.lower().startswith('win'):
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-ADMINS = (('Olivier Pons', 'olivier.pons@hqf.fr'), )
+ADMINS = (('Olivier Pons', 'olivier.pons@hqf.fr'),)
 DEFAULT_FROM_EMAIL = 'contact@hqf.fr'
 
 WEBSITE_NAME = 'evalr'
@@ -196,7 +286,7 @@ AUTHENTICATION_BACKENDS = (
 # tutorial/2016/10/24/how-to-add-social-login-to-django.html
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
-  'fields': 'name, email, age_range'
+    'fields': 'name, email, age_range'
 }
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '120033307782-epmpr28telvcolh8osqp9l7' \
@@ -225,8 +315,8 @@ STATICFILES_FINDERS = (
 )
 
 # base = 2621440 = 2.5MB of data in memory, I accept 25MB:
-DATA_UPLOAD_MAX_MEMORY_SIZE = 2621440*10
-FILE_UPLOAD_MAX_MEMORY_SIZE = 2621440*10
+DATA_UPLOAD_MAX_MEMORY_SIZE = 2621440 * 10
+FILE_UPLOAD_MAX_MEMORY_SIZE = 2621440 * 10
 
 # Aahahaha -> merci stackoverflow !
 # https://docs.djangoproject.com/en/2.2/ref/settings/#append-slash
@@ -250,7 +340,6 @@ LOGIN_REDIRECT_URL = 'app_index'
 
 # where to redirect after logout (ignored because I've made my own Logout view):
 LOGOUT_REDIRECT_URL = '/'
-
 
 # -----------------------------------------------------------------------------
 # internationalization
